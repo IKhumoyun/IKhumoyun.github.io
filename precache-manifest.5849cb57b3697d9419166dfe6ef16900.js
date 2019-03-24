3163eb7e61ef93febbd3c58de8e48102185a4c3a
Khumoyun Inoyatov <khumoyun@inoyatov.com>
2019-03-24 15:00:21 +0500

    BBuck front

---
+++ precache-manifest.5849cb57b3697d9419166dfe6ef16900.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "588c161a91340101dc74112cb2c7d497",
+    "url": "/static/media/arrow_down.588c161a.svg"
+  },
+  {
+    "revision": "230b4740529c13bff0a1",
+    "url": "/static/css/main.3a815edc.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c398c9e4cc3690725024",
+    "url": "/static/js/2.34754898.chunk.js"
+  },
+  {
+    "revision": "2cd7a178622733f149e733e360d85e60",
+    "url": "/static/media/logo.2cd7a178.svg"
+  },
+  {
+    "revision": "6975341d1bc4004fa63d782857e1cfa9",
+    "url": "/static/media/header_logo.6975341d.svg"
+  },
+  {
+    "revision": "230b4740529c13bff0a1",
+    "url": "/static/js/main.cdcecfdf.chunk.js"
+  },
+  {
+    "revision": "04a3a1259409cdbc1c226a7a248d42fb",
+    "url": "/static/media/profile.04a3a125.svg"
+  },
+  {
+    "revision": "3c3de7fc31df00410b2c0bc75d1c286f",
+    "url": "/static/media/white_logo.3c3de7fc.svg"
+  },
+  {
+    "revision": "1a8b4b24567ebe90cb56fa90badd6790",
+    "url": "/static/media/fortnite.1a8b4b24.svg"
+  },
+  {
+    "revision": "a3934f1cd2e118f286ceed59d459263e",
+    "url": "/static/media/csgo.a3934f1c.svg"
+  },
+  {
+    "revision": "637af3cdaa37fc5d5833d90c07311ca7",
+    "url": "/static/media/pubg.637af3cd.svg"
+  },
+  {
+    "revision": "8901739165a5419f318aa158a9c891cd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cec6ddbe5f4abb41c595e5132bd3049.js"
+  "/precache-manifest.5849cb57b3697d9419166dfe6ef16900.js"
 );
 
 workbox.clientsClaim();
